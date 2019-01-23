2ce50861fc7240a6b7f401797e4768d17b8013b0
ZhaoHuiXin <zhaohuxin@autoforce.net>
2019-01-23 18:47:04 +0800

    remove debug lg

--- db.go
+++ db.go
@@ -3,14 +3,12 @@ package main
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"log"
 )
 
 func (p *App) OpenDB() (err error) {
 	if p.db != nil {
 		return
 	}
-	log.Println(p.dbDsn)
 	db, err := gorm.Open("mysql", p.dbDsn + "?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
 		return err
